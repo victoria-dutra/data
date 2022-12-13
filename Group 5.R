a88fd16f842bad0086383e8c91716e2bc3c8ce19
oussama <oussama.yami16@gmail.com>
2022-12-13 13:21:14 +0100

    no changes

--- Group 5.R
+++ Group 5.R
@@ -1,7 +1,39 @@
 # Group 5 - Water stress experiment
+<<<<<<< HEAD
 # install.packages("readxl")
 readxl::read_excel("data_dec/water_stress.xlsx")
 d0 <- readxl::read_excel("data_dec/water_stress.xlsx",sheet = "Data")
 
 
 # just trying
+=======
+## install.packages("readxl")
+readxl::read_excel("data_dec/water_stress.xlsx")
+d0 <- readxl::read_excel("data_dec/water_stress.xlsx",sheet = "Data")
+
+## Sheet organization
+# 1260 observations
+# 23 variables
+
+## Starting plots
+### Each species
+# S1 : Solanum lycopersicum
+
+# S2 : Amaranthus retroflexus
+
+# S3 : Spinacia oleracea
+
+# S4 : Amaranthus retroflexus
+
+# S5 : Hordeum vulgare
+
+# S6 : Lolium perenne
+
+# S7 : Beta vulgaris
+
+# S8 : Sonchus oleraceus
+
+# S9 : Portulacea oleracea
+
+# S10 : Raphanus sativus
+>>>>>>> e226e731ee248cb8b65d8b5777c278ae04a11745
