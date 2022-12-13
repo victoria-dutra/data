7fa680425ead43c34743a2196bfbdb9f44f4bc8e
victoria-dutra <victoriadutratomas1@gmail.com>
2022-12-13 12:15:50 +0100

    Markdown

--- Group 5.R
+++ Group 5.R
@@ -1,5 +1,4 @@
 # Group 5 - Water stress experiment
-install.packages("readxl")
+## install.packages("readxl")
 readxl::read_excel("data_dec/water_stress.xlsx")
 d0 <- readxl::read_excel("data_dec/water_stress.xlsx",sheet = "Data")
-
