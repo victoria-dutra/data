d262d075046aa5ec22a009cfcf45035493995769
victoria-dutra <victoriadutratomas1@gmail.com>
2022-12-13 12:21:17 +0100

    first

--- Group 5.R
+++ Group 5.R
@@ -2,3 +2,29 @@
 ## install.packages("readxl")
 readxl::read_excel("data_dec/water_stress.xlsx")
 d0 <- readxl::read_excel("data_dec/water_stress.xlsx",sheet = "Data")
+
+## Sheet organization
+# 1260 observations
+# 23 variables
+
+## Starting plots
+### Each species
+# S1 : Solanum lycopersicum
+
+# S2 : Amaranthus retroflexus
+
+# S3 : Spinacia oleracea
+
+# S4 : Amaranthus retroflexus
+
+# S5 : Hordeum vulgare
+
+# S6 : Lolium perenne
+
+# S7 : Beta vulgaris
+
+# S8 : Sonchus oleraceus
+
+# S9 : Portulacea oleracea
+
+# S10 : Raphanus sativus
